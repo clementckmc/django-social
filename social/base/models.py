7ca6e452dd9c22fc8cbf95807831d0941d32aea7
Clement Choi <clementchoi1997@gmail.com>
2023-01-03 21:34:55 +0900

    admin and model amend

--- social/base/models.py
+++ social/base/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
