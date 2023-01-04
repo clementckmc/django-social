38a03eafbbb486f6027b8ffd6cdf17005fddb84e
Clement Choi <clementchoi1997@gmail.com>
2023-01-04 20:23:04 +0900

    login, logout, register

--- base/forms.py
+++ base/forms.py
@@ -6,3 +6,8 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
--- base/views.py
+++ base/views.py
@@ -1,9 +1,52 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            HttpResponse('invalid Username or password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            HttpResponse('invalid Username or password')
+
+    return render(request, 'base/login_register.html', {'page': page})
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse('An error occurred during registration')
+    return render(request, 'base/login_register.html', {'form': form})
+
 def home(request):
     posts = Post.objects.all()
     context = {'posts': posts}
