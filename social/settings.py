e2e210e8678cb2e836eebfbf08579dcea03fb140
Clement Choi <clementchoi1997@gmail.com>
2023-01-08 23:22:03 +0900

    DB

--- social/settings.py
+++ social/settings.py
@@ -184,3 +184,5 @@ cloudinary.config(
 # Configure Django App for Heroku.
 import django_on_heroku
 django_on_heroku.settings(locals())
+
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
