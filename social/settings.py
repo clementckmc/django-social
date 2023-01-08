0b1895daf144e49aae00974d2d83d43d0402260f
Clement Choi <clementchoi1997@gmail.com>
2023-01-08 22:59:13 +0900

    database setting

--- social/settings.py
+++ social/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'social.wsgi.application'
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
